291ffa6b00a70c5da07c0216ca0815ab1b736a9b
zidiu5 <147642178+zidiu5@users.noreply.github.com>
2026-04-19 20:50:55 +0200

    Remove old Pastebin loading code

--- ZidiuUI.lua
+++ ZidiuUI.lua
@@ -1,16 +1,1 @@
 local PastebinURL = "https://pastebin.com/raw/e3bYUdLj"
-
-local success, result = pcall(function()
-    return game:HttpGet(PastebinURL)
-end)
-
-if success then
-    local func = loadstring(result)
-    if func then
-        func()
-    else
-        warn("Error: Loadstring failed")
-    end
-else
-    warn("Error by Loading from Pastebin:", result)
-end
